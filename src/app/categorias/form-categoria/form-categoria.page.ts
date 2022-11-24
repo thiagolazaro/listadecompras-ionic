d2d05cd85dc77aa0e856e8af83e175bc3c52c1b5
Thiago Lázaro <thiagolazaro2009@gmail.com>
2022-11-24 14:58:32 -0300

    Finalizando a form-categoria

--- src/app/categorias/form-categoria/form-categoria.page.ts
+++ src/app/categorias/form-categoria/form-categoria.page.ts
@@ -1,4 +1,8 @@
+import { ToastService } from 'src/app/core/service/toast.service';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Categoria } from '../shared/categoria';
+import { CategoriaService } from '../shared/categoria.service';
 
 @Component({
   selector: 'app-form-categoria',
@@ -6,10 +10,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form-categoria.page.scss'],
 })
 export class FormCategoriaPage implements OnInit {
+  titulo: string = 'Nova categoria';
+  categoria!: Categoria;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private toast: ToastService,
+    private categoriaService: CategoriaService
+  ) { }
 
   ngOnInit() {
+    this.categoria = new Categoria();
+
+    const idParam = this.route.snapshot.paramMap.get('id');
+    if (idParam) {
+      this.titulo = 'Editar categoria';
+      this.carregaCategoria(parseInt(idParam));
+    }
+  }
+
+  carregaCategoria(id: number) {
+    return new Categoria();
+  }
+
+  onSubmit() {
+    console.log(this.categoria);
+    this.toast.success('Categoria salva com sucesso');
   }
 
 }
