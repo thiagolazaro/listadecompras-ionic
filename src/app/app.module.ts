edc5583bc497fbaf06f0f9e76d0338e33939e969
Thiago Lázaro <thiagolazaro2009@gmail.com>
2022-11-30 23:04:58 -0300

    Crindo validação personalizada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,9 @@ import { SQLitePorter } from '@awesome-cordova-plugins/sqlite-porter/ngx';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MinValueDirective } from './core/validation/min-value.directive';
 
 @NgModule({
-  declarations: [AppComponent, MinValueDirective],
+  declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, SQLite, SQLitePorter],
   bootstrap: [AppComponent],
--- src/app/core/validation/min-value.directive.ts
+++ src/app/core/validation/min-value.directive.ts
@@ -1,10 +1,25 @@
-import { Directive } from '@angular/core';
+import { Directive, Input } from '@angular/core';
+import { NG_VALIDATORS, Validator, AbstractControl, ValidatorFn, ValidationErrors } from '@angular/forms';
 
 @Directive({
-  selector: '[appMinValue]'
+  selector: '[appMinValue]',
+  providers: [{provide: NG_VALIDATORS, useExisting: MinValueDirective, multi: true}]
 })
-export class MinValueDirective {
+
+export class MinValueDirective implements Validator {
+  @Input('appMinValue') minValue!: number;
 
   constructor() { }
 
+  validate(control: AbstractControl): {[key: string]: any}| null {
+    return this.minValue ? minValueValidator(this.minValue)(control) : null;
+  }
+
+}
+
+export function minValueValidator(minValue: number): ValidatorFn {
+  return (control: AbstractControl): {[key: string]: any} | null => {
+    const isValid = minValue <= control.value;
+    return isValid ? null : { 'appMinValue': true};
+  }
 }
